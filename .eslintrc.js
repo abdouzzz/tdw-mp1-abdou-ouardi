c3b41c81c7b62bdbe3653037ca572e2ea450f4ef
abdouzzz <abdououardi3@gmail.com>
2025-10-13 11:46:05 +0100

    chore: update ESLint and Prettier configurations, add TypeScript support, and standardize quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,25 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
+  root: true,
+  parser: '@typescript-eslint/parser', // Pour parser TS
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module', // support ES modules
+  },
+  env: {
+    node: true, // pour 'module' et 'require'
+    browser: false,
+    es2021: true,
+    jest: true, // si tu utilises Jest
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended', // Prettier doit être le dernier
+  ],
   rules: {
     'prettier/prettier': 'error',
+    // ajoute tes règles spécifiques ici
   },
+  ignorePatterns: ['node_modules/', 'dist/'], // fichiers à ignorer
 };
